5abdf5cbc45d2b3ffc9b75e20e54c04166ac551c
kailani <kailani4@icloud.com>
2026-08-13 16:02:06 -0700

    utility

--- R/util.R
+++ R/util.R
@@ -1,18 +1,7 @@
-reef <- matrix(0, nrow = 5, ncol = 5)
-
-rc <- sample(1:25, 8)
-for(i in rc){
-    rc_row <- (i - 1) %/% 5 + 1
-    rc_col <- (i - 1) %% 5 + 1
-    reef[rc_row, rc_col] <- 1
-}
-print(reef)
-
-
-#reef <- random_corals(init_reef, rc, rc_row, rc_col)
+# my answer
 place_coral <- function(coral) {
   reef <- matrix(0, nrow = 5, ncol = 5)
-  rc <- sample(1:25, size = coral, replace = FALSE)
+  rc <- sample(1:25, size = coral)
   for(i in rc){
     rc_row <- (i - 1) %/% 5 + 1
     rc_col <- (i - 1) %% 5 + 1
@@ -20,4 +9,22 @@ place_coral <- function(coral) {
   }
   return(reef)
 }
-place_coral(20)
+place_coral(8)
+
+
+# class answer
+# initialize_reef <- function(coral_numbers) {
+#   reef <- matrix(0, nrow = 5, ncol = 5)
+
+#   random_coral <- sample(1:25, size = coral_numbers)
+ 
+#   for(rc in random_coral){
+#     rc_row <- (rc - 1) %/% 5 + 1
+#     rc_col <- (rc - 1) %% 5 + 1
+#     reef[rc_row, rc_col] <- 1
+#   }
+
+#   return(reef)
+# }
+
+# initialize_reef(coral_numbers = 8)
